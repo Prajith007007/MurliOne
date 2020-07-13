76b07a1b190b0b5837132343417388e9f2d16aa0
Prajith007007 <prajithprasad112@gmail.com>
2020-07-13 06:16:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/prajith/murlione/DailyMurli.java
@@ -0,0 +1,63 @@
+package com.example.prajith.murlione;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DailyMurli extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
+    private WebView webView;
+    String year, month, day;
+    DatePicker picker;
+    Button button;
+    Dialog myDialog;
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myDialog= new Dialog(this);
+        myDialog.setContentView(R.layout.custompopup);
+        setContentView(R.layout.activity_daily_murli);
+        myDialog = new Dialog(this);
+        webView = (findViewById(R.id.webview));
+       picker = (DatePicker) findViewById(R.id.datePicker1);
+       button = (Button) findViewById(R.id.pick);
+       // button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               picker.setVisibility(View.GONE);
+               button.setVisibility(View.GONE);
+                webView.setWebViewClient(new WebViewClient());
+                webView.getSettings().setDomStorageEnabled(true);
+              //  webView.loadUrl("http://madhubanmurli.org/#ml+2019-07-29");
+                webView.loadUrl("http://madhubanmurli.org/#ml+" + picker.getYear() + "-0" + (picker.getMonth() + 1) + "-" + picker.getDayOfMonth());
+                Toast.makeText(DailyMurli.this, "Loading Malayalam Murli +"+ picker.getYear() + "-0" + (picker.getMonth() + 1) + "-" + picker.getDayOfMonth(),
+                        Toast.LENGTH_SHORT).show();
+                WebSettings webSettings = webView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/prajith/murlione/EnableLive.java
@@ -0,0 +1,40 @@
+package com.example.prajith.murlione;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class EnableLive extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enable_live);
+        final Button button = findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCFoXdXGQG1KWiNNIVKkXL1Q")));
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCFoXdXGQG1KWiNNIVKkXL1Q")));
+            }
+        });
+        final Intent intent = new Intent(this,DailyMurli.class);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+               startActivity(intent);
+                // startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCFoXdXGQG1KWiNNIVKkXL1Q")));
+                Log.i("Video", "Video Playing...."); // Code here executes on main thread after user presses button
+            }
+        });
+
+    }
+}
